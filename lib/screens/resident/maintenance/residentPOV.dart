e6097a04d032e4ca3b59784c328aa04796e9a677
nsp456 <n.pal@somaiya.edu>
2022-01-21 00:05:15 +0530

    minor changes in maintenance

--- lib/screens/resident/maintenance/residentPOV.dart
+++ lib/screens/resident/maintenance/residentPOV.dart
@@ -1,10 +1,7 @@
+import 'package:ease_it/screens/resident/maintenance/transactionHistory.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'package:ease_it/firebase/database.dart';
-import 'package:ease_it/utility/loading.dart';
-import 'package:ease_it/screens/resident/resident.dart';
-import 'package:ease_it/screens/security/security.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ease_it/utility/globals.dart';
 
 class ResidentPOV extends StatefulWidget {
@@ -14,11 +11,40 @@ class ResidentPOV extends StatefulWidget {
   _ResidentPOVState createState() => _ResidentPOVState();
 }
 
+Globals g = Globals();
+
 class _ResidentPOVState extends State<ResidentPOV> {
+  final db = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return DefaultTabController(
+    length: 1,
+    child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(
+          'Maintenance',
+          style: GoogleFonts.sourceSansPro(
+              fontSize: 25, fontWeight: FontWeight.w900),
+        ),
+        SizedBox(height: 15),
+        TabBar(
+            indicatorColor: Color(0xff1a73e8),
+            labelColor: Colors.black,
+            indicatorWeight: 2.5,
+            labelStyle: GoogleFonts.sourceSansPro(
+                fontSize: 16, fontWeight: FontWeight.w600),
+            tabs: [
+              Tab(
+                text: 'My Maintenance History',
+              )
+            ]),
+        Expanded(
+          child: TabBarView(children: [
+            TransactionHistory(),              
+          ]),
+        ),
+      ]),
+    ));
   }
 }
