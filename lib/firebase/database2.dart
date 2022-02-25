caa8140a3989f53352cb3d4d24b980e7ac98d2cd
Yash4900 <satra.y@somaiya.edu>
2022-02-25 12:20:50 +0530

    daily helper functionality security side

---
+++ lib/firebase/database2.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Database2 {
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<void> addDailyHelper(String society, String name, String phoneNum,
+      List<String> worksAt, String imageUrl, String purpose, int code) async {
+    try {
+      await _firestore
+          .collection(society)
+          .doc('dailyHelpers')
+          .collection('Daily Helper')
+          .add({
+        'name': name,
+        'phoneNum': phoneNum,
+        'worksAt': worksAt,
+        'imageUrl': imageUrl,
+        'purpose': purpose,
+        'code': code
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
